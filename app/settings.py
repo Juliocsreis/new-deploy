0aee5e45673328fbaeff50be04ce36417d7fb69d
Julio <juliocsreis@gmail.com>
2019-08-27 11:05:18 -0300

    adcionado import psycopg2 e databaseurl

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,7 +80,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES= {'default': {dj_database_url.config(conn_max_age=600, ssl_require=True)}} 
+DATABASES= {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 #DATABASES = {
 #    'default': {
