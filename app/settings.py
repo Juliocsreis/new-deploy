55f36662c89a9d7990dd970514aa7e4f7a9e8cf1
Julio <juliocsreis@gmail.com>
2019-08-27 10:57:33 -0300

    databases codigo alterado conforme intuição

--- app/settings.py
+++ app/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES= {'default': {dj_database_url.config(conn_max_age=600, ssl_require=True)}} 
 
 #DATABASES = {
 #    'default': {
