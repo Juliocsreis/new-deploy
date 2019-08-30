7295886679c94a255f03d185db28b05cc5be477d
Julio <juliocsreis@gmail.com>
2019-08-30 11:35:57 -0300

    teste recipe api create user

--- app/settings.py
+++ app/settings.py
@@ -45,7 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
